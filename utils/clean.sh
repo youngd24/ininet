93e56d5678caab3e548362d7954f119e283f3198
Darren Young <youngd24@gmail.com>
2025-07-22 17:36:04 -0500

    cleaned up keys

--- utils/clean.sh
+++ utils/clean.sh
@@ -5,5 +5,9 @@ tftpdir="/srv/tftp/ininet"
 for file in `cd $tftpdir && ls *conf*`;
 do
     echo "Cleaning $tftpdir/$file"
-    cat $tftpdir/$file | grep -v username | grep -v 'enable pass' | grep -v 'enable sec' > ../configs/$file
+    cat $tftpdir/$file | \
+	    grep -v username | \
+	    grep -v 'enable pass' | \
+	    grep -v 'enable sec' | \
+	    grep -v 'tacacs-server key' > ../configs/$file
 done

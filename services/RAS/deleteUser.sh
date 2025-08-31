7b7535c4955799953e88e376a14fb95ee3a0460c
Darren Young <youngd24@gmail.com>
2025-08-31 14:18:06 -0500

    added delete user script

---
+++ services/RAS/deleteUser.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+##############################################################################
+#
+# createUser.sh
+#
+# Create a RAS (AIM) user via the API
+#
+##############################################################################
+
+# Gotta give us sumpin to go on
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <screen_name>"
+    exit 1
+fi
+
+SCREEN_NAME="$1"
+
+# Debugging: show what is being sent
+echo "Using screen name: '$SCREEN_NAME'"
+
+# First API call
+curl -s -H "Content-Type: application/json" \
+     -X DELETE -d "{\"screen_name\":\"$SCREEN_NAME\"}" \
+     http://localhost:8080/user
+

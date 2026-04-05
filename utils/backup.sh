75cd7b6099dd03290cb5a2247b875bee9df761a0
Darren Young <youngd24@gmail.com>
2026-04-05 03:57:31 -0500

    started backup script

--- utils/backup.sh
+++ utils/backup.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 #
 
-./getconfig.sh
-./clean.sh
+WORKDIR=$(pwd)
+BASEDIR=$(dirname "$WORKDIR")
+CONFIGDIR="$BASEDIR/configs"
+LOGFILE="$WORKDIR/backup.log"
+
+echo "WORKDIR: $WORKDIR"
+echo "CONFIGDIR: $CONFIGDIR"
+echo "LOGFILE: $LOGFILE"
+
+echo "Gathering configs"
+$WORKDIR/getconfigs.exp
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+    echo "FAILED TO GET CONFIGS"
+    exit 1
+else
+    echo "SUCCESSFULLY GATHERED CONFIGS"
+fi
+
+echo "Cleaning out sensitive and unneeded aspects"
+$WORKDIR/clean.sh
+RESULT=$?
+if [[ $RESULT -ne 0 ]]; then
+    echo "FAILED TO CLEAN CONFIGS"
+    exit 1
+else
+    echo "SUCCESSFULLY CLEANED CONFIGS"
+fi
